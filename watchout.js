31a72ae76be0f6a540d1001f56013ef1cbdf65ca
Adam Price <aesprice@gmail.com>
2014-06-14 12:04:24 -0700

    Created enemies based on an array of objects Added a setInterval to make enemies move every second

--- watchout.js
+++ watchout.js
@@ -1,2 +1,42 @@
-// start slingin' some d3 here.
+var settings = {
+  width: 640,
+  height: 480,
+  enemyCount: 10
+}
 
+var enemyData = [];
+
+for(var i = 0; i < settings.enemyCount; i++){
+  enemyData.push({
+    x: Math.random() * settings.width,
+    y: Math.random() * settings.height
+  });
+}
+
+var gameBoard = d3.select('body').append('svg')
+  .attr('class', 'gameBoard')
+  .attr('width', settings.width)
+  .attr('height', settings.height);
+
+var enemies = gameBoard.selectAll('circle').data(enemyData)
+  .enter().append('circle')
+    .attr('class', 'enemy')
+    .attr('cx', function(item){return item.x;})
+    .attr('cy', function(item){return item.y;})
+    .attr('r', 10);
+
+var enemyTurn = function(){
+  for(var i = 0; i < enemyData.length; i++){
+    enemyData[i].x = Math.random() * (settings.width);
+    enemyData[i].y = Math.random() * (settings.height);
+  }
+}
+
+setInterval(function(){
+  enemyTurn();
+  enemies
+    .transition()
+      .duration(1000)
+      .attr('cx', function(item){return item.x;})
+      .attr('cy', function(item){return item.y;})
+}, 1000)
