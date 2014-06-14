cdd23c17aed7d66dd90e798696948e2f53dd7939
Adam Price <aesprice@gmail.com>
2014-06-14 12:24:50 -0700

    Added player and a border to the gameBoard

--- watchout.js
+++ watchout.js
@@ -4,9 +4,13 @@ var settings = {
   enemyCount: 10
 }
 
-var enemyData = [];
+var playerData = [{
+  x: settings.width/2,
+  y: settings.height/2
+}]
 
-for(var i = 0; i < settings.enemyCount; i++){
+var enemyData = [];
+for(var i = 0; i <= settings.enemyCount; i++){
   enemyData.push({
     x: Math.random() * settings.width,
     y: Math.random() * settings.height
@@ -17,6 +21,24 @@ var gameBoard = d3.select('body').append('svg')
   .attr('class', 'gameBoard')
   .attr('width', settings.width)
   .attr('height', settings.height);
+
+gameBoard.append('rect')
+  .attr('x', 0)
+  .attr('y', 0)
+  .attr('width', settings.width)
+  .attr('height', settings.height)
+  .style('stroke', 'gray')
+  .style('fill', 'none')
+  .style('stroke-width', 1);
+
+
+var player = gameBoard.selectAll('circle').data(playerData)
+  .enter().append('circle')
+    .attr('class', 'player')
+    .attr('fill', 'crimson')
+    .attr('cx', function(item){return item.x})
+    .attr('cy', function(item){return item.y})
+    .attr('r', 10);
 
 var enemies = gameBoard.selectAll('circle').data(enemyData)
   .enter().append('circle')
